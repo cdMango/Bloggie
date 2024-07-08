32adb75b55ca21696e395b3270dc3cffce7657f4
mango <zand951@UoA.auckland.ac.nz>
2024-07-08 22:17:10 +1200

    d

--- Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecea7f8ace3b34a3685ae5f5aae7106074f61df6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966b30187fc71f521e298e245114f4222a2459bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
