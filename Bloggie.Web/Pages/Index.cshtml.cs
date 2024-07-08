0a4e57c0c67afe81e8a37069c78a51802ecc130d
mango <zand951@UoA.auckland.ac.nz>
2024-07-08 22:37:04 +1200

    Displaying all tags for all articles on homepage just below the "Blogs" heading

--- Bloggie.Web/Pages/Index.cshtml.cs
+++ Bloggie.Web/Pages/Index.cshtml.cs
@@ -9,17 +9,23 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IBlogPostRepository _blogPostRepository;
+    private readonly ITagRepository _tagRepository;
     
     public List<BlogPost> Blogs { get; set; }
-    public IndexModel(ILogger<IndexModel> logger, IBlogPostRepository blogPostRepository)
+    public List<Tag> Tags { get; set; }
+    public IndexModel(ILogger<IndexModel> logger,
+        IBlogPostRepository blogPostRepository,
+        ITagRepository tagRepository)
     {
         _logger = logger;
         _blogPostRepository = blogPostRepository;
+        _tagRepository = tagRepository;
     }
 
     public async Task<IActionResult>  OnGet()
     {
         Blogs =(await _blogPostRepository.GetAllAsync()).ToList();
+        Tags = (await _tagRepository.GetAllAsync()).ToList(); 
         return Page(); 
     }
 }
---
+++ Bloggie.Web/Repositories/TagRepository.cs
@@ -0,0 +1,23 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Page = CloudinaryDotNet.Actions.Page;
+
+namespace Bloggie.Web.Repositories;
+
+public class TagRepository : ITagRepository
+{
+    private readonly BloggieDbContext _bloggieDbContext;
+
+    public TagRepository(BloggieDbContext bloggieDbContext)
+    {
+        _bloggieDbContext = bloggieDbContext;
+    }
+    public async Task<IEnumerable<Tag>> GetAllAsync()
+    {
+        var tags = await _bloggieDbContext.Tags.ToListAsync();
+
+        return tags.DistinctBy(x => x.Name.ToLower()); 
+    }
+}
