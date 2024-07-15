387a6e68fe801ce7350a138ebbdb9aef31b30c8b
cdMango <cdMango@proton.me>
2024-07-15 14:30:27 +1200

    Dependency Injection Example (signInManager) made it so that only logged in users have the ability to like a post

--- Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7042feca5b1b47a9025a369580993689087984e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f64fd2b638f46d96b1f41fa9dff3511188605de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
